0197e0bf5264cd96bcb17f1cf44ce062a071fed1
wilfredkesh <wilfredkesh5@gmail.com>
2017-04-08 11:49:05 -0400

    added sengrid email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,11 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'wilfredgish-saasproject-app.herokuapp.com', :protocol => 'https' }   
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
